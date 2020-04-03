9c90e4ba7ddfd3ca0e863b4f3187aeaab9b06a35
kryvets03 <vadimkrvts03@gmail.com>
2020-04-04 00:41:07 +0300

    Additional task

--- 2.2.py
+++ 2.2.py
@@ -17,7 +17,7 @@ def getValues(n, N, W):
         w = W - i * W / (n)
         x = A * np.sin(np.arange(0, N, 1) * w + fi)
         generated_signal += x
-    print(f"Execution time: {time.time() - start}")
+    # print(f"Execution time: {time.time() - start}")
     return generated_signal
 
 def draw(arr, x_label, y_label, title, legend, file_name=None):
@@ -34,10 +34,10 @@ def dft(signal):
     for p in range(N):
         spectre[p] = np.dot(signal, np.cos(2 * math.pi * p / N * np.linspace(0, N-1, N))) \
                      -1j * np.dot(signal, np.sin(2 * math.pi * p / N * np.linspace(0, N-1, N)))
-    print(f"Execution time: {time.time() - start}")
+    print(f"Execution DFT time: {time.time() - start}")
     return spectre
 
-signal = getValues(6, 256, 2100)
+signal = getValues(6, 256, 2048)
 
 spectr = dft(signal)
 polar_spectr = np.array(list(map(lambda x: cmath.polar(x), spectr)))
@@ -49,6 +49,7 @@ phase = draw(polar_spectr[:, 1], "p", "Phi(p)", "Polar Spectr", "Phase")
 plt.legend(handles=[phase], loc='upper right')
 plt.grid()
 plt.show()
+
 
 def fft(signal):
     start = time.time()
@@ -62,7 +63,7 @@ def fft(signal):
               np.sin(2 * math.pi * p / (N / 2) * np.arange(0, N // 2, 1)))
         spectre[p] = E_m + W_p * O_m
         spectre[p + N // 2] = E_m - W_p * O_m
-    print(f"Execution time: {time.time() - start}")
+    print(f"Execution FFT time: {time.time() - start}")
     return spectre
 
 spectr = fft(signal)
@@ -75,3 +76,29 @@ phase = draw(polar_spectr[:, 1], "p", "Phi(p)", "Polar Spectr", "Phase")
 plt.legend(handles=[phase], loc='upper right')
 plt.grid()
 plt.show()
+
+
+# Additional task (Table method)
+def make_table(signal):
+    start = time.time()
+    N = len(signal)
+    table = np.cos(2 * math.pi / N * np.linspace(0, N-1, N)) \
+            -1j * np.sin(2 * math.pi / N * np.linspace(0, N-1, N))
+    spectre = np.zeros(N, dtype=np.complex64)
+    for p in range(N):
+        indicies = np.linspace(0, N-1, N, dtype=np.int32) * p % N
+        spectre[p] = np.dot(signal, table[indicies])
+    print(f"Execution MakeTable time: {time.time() - start}")
+    return spectre
+
+spectr = make_table(signal)
+polar_spectr = np.array(list(map(lambda x: cmath.polar(x), spectr)))
+ampl = draw(polar_spectr[:, 0], "p", "A(p)", "Polar Spectr", "Amplitude")
+plt.legend(handles=[ampl], loc='upper right')
+plt.grid()
+plt.show()
+phase = draw(polar_spectr[:, 1], "p", "Phi(p)", "Polar Spectr", "Phase")
+plt.legend(handles=[phase], loc='upper right')
+plt.grid()
+plt.show()
+
